b2dc9944a49e25572dd3f4654f029f186424ef97
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-04-17 20:39:45 +1200

    Added a global script with fruit counters

---
+++ src/Scripts/Global.gd
@@ -0,0 +1,18 @@
+extends Node
+# This is a global script, it is not attached to anything and can be accessed from any script
+
+# this list may change later
+enum {
+	APPLE,
+	BANANA,
+	GRAPE,
+}
+
+# This array will hold the counter for each fruit
+onready var fruit_counter = []
+
+
+# This function will be called to increment certain fruit counts
+# to use in another script e.g. Global.increment_fruit(Global.APPLE)
+func increment_fruit(fruit_type: int) -> void:
+	fruit_counter[fruit_type] += 1
