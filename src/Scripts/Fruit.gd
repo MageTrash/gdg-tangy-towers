a6fb415d5d90f31b78b6a756b8c02d27db1e2593
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-04-17 21:01:35 +1200

    added fruit scene and testing scene

---
+++ src/Scripts/Fruit.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+export(Global.fruit) var fruit_type = Global.fruit.APPLE
+
+
+onready var area = $PickupArea
+
+func _ready() -> void:
+	area.connect("body_entered", self, "on_area_entered")
+
+
+func on_area_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		print("maybe prompt for pick up? or just pick it up straight away")
--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -2,7 +2,7 @@ extends Node
 # This is a global script, it is not attached to anything and can be accessed from any script
 
 # this list may change later
-enum {
+enum fruit {
 	APPLE,
 	BANANA,
 	GRAPE,
@@ -13,6 +13,6 @@ onready var fruit_counter = []
 
 
 # This function will be called to increment certain fruit counts
-# to use in another script e.g. Global.increment_fruit(Global.APPLE)
+# to use in another script e.g. Global.increment_fruit(Global.fruit.APPLE)
 func increment_fruit(fruit_type: int) -> void:
 	fruit_counter[fruit_type] += 1
