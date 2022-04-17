a8c9833d9ab6cde30e326b0a78539a6a5593cf6b
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-04-17 21:29:10 +1200

    Update Global.gd

--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -11,6 +11,13 @@ enum fruit {
 # This array will hold the counter for each fruit
 onready var fruit_counter = []
 
+func _ready() -> void:
+	randomize()
+
+	# This will add/set all the counters to zero at the start of the game
+	for i in len(fruit.values()):
+		fruit_counter.append(0)
+
 
 # This function will be called to increment certain fruit counts
 # to use in another script e.g. Global.increment_fruit(Global.fruit.APPLE)
