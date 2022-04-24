e20015227796ff6d6b8116eb647ae9f35118d4d4
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-04-25 00:17:29 +1200

    place tower

--- src/Scripts/TestLabel.gd
+++ src/Scripts/TestLabel.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 	Global.player.connect("building", self, "build_mode")
 
 func build_mode(is_building) -> void:
-	var message = "Build Mode: "
+	var message = "Place Towers?: "
 	text = message + str("on" if is_building else "off")
