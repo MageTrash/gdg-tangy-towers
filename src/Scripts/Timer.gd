6313f0df72894ad8df3efa203ec628d9e11305a5
Mage <mistfist75@gmail.com>
2022-04-30 14:29:24 +1200

    quick hotfix

--- src/Scripts/Timer.gd
+++ src/Scripts/Timer.gd
@@ -1,8 +1,8 @@
 extends Label
 
-export(float, 0.0, 120.0) var time = 120.0
+export(float, 0.00, 120.00) var time = 120.00
 
 func _process(delta):
 	time -= delta
-	var time_display = stepify(time, 0.1)
+	var time_display = stepify(time, 0.01)
 	text = var2str(time_display)
