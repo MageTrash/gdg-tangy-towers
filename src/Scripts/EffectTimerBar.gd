7bf6e6fcf558e3c5a5904a753ddeff3706a83745
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-05-08 16:57:15 +1200

    added effect timer progress bar UI

--- src/Scripts/EffectTimerBar.gd
+++ src/Scripts/EffectTimerBar.gd
@@ -1,17 +1,6 @@
 extends ProgressBar
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-#	self.value = Global.effect_timer
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta: float) -> void:
+	max_value = Global.effect_timer.wait_time
+	value = Global.effect_timer.time_left
