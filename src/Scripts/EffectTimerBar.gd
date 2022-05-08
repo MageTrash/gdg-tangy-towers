4bed3675343962b8eba2e5b54ec5325bd26390e4
JK03Coder <56343148+JK03Coder@users.noreply.github.com>
2022-05-08 16:59:02 +1200

    Update EffectTimerBar.gd

--- src/Scripts/EffectTimerBar.gd
+++ src/Scripts/EffectTimerBar.gd
@@ -1,6 +1,11 @@
 extends ProgressBar
 
 
+func _ready() -> void:
+	value = 0.0
+
+
 func _process(_delta: float) -> void:
-	max_value = Global.effect_timer.wait_time
-	value = Global.effect_timer.time_left
+	if not Global.effect_timer.is_stopped():
+		max_value = Global.effect_timer.wait_time
+		value = Global.effect_timer.time_left
