d2da11888a5b48d2f82ed89ad4b1b4416f5ee4c4
Ghostpengi <47687140+Ghostpengi@users.noreply.github.com>
2022-05-09 14:02:41 +1200

    Adding a sfx controller
    Adding the SFX controller and its SFX. SFX added are tower placement and enemy death SFX.

--- src/AutoLoad/SFXMan.gd
+++ src/AutoLoad/SFXMan.gd
@@ -1,22 +1,20 @@
 extends Node
 
 
-var menu_track = load("res://Art n Music/Music/TT_Menu.wav")
-var gameplay_track = load("res://Art n Music/Music/TT_Gameplay.wav")
 var tower_placeSFX = load("res://Art n Music/SFX/towerPlaceSound.wav")
 var enemy_deathSFX = load("res://Art n Music/SFX/fruitDeathSound.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func play_menutrack():
-	$Music.stream = menu_track
+func play_towerplacesfx():
+	$Music.stream = tower_placeSFX
 	$Music.play()
 	
-func play_gameplaytrack():
-	$Music.stream = gameplay_track
+func play_enemydeathsfx():
+	$Music.stream = enemy_deathSFX
 	$Music.play()
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
